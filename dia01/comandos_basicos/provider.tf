bb706ae273d77003a0c5ef300863275f95c0f176
Lukas Veiga <lukas.veiga@hotmail.com>
2025-01-18 12:26:50 -0300

    feat: commit da aula de comandos basicos

---
+++ dia01/comandos_basicos/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  region = "us-east-2"
+  profile = "descomplicando_terraform"
+}
