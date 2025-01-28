f90598e7602bcc4aac6b77cbaa585b9f1bb3d1f4
Lukas Veiga <lukas.veiga@hotmail.com>
2025-01-27 22:13:57 -0300

    feat: state and locking state

---
+++ dia02/state/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket  = "descomplicando-terraform-veiga"
+    key     = "aula_backend"
+    region  = "us-east-2"
+    profile = "descomplicando_terraform"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  region                   = "us-east-2"
+  profile                  = "descomplicando_terraform"
+}
+
+provider "aws" {
+  alias                    = "west"
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  region                   = "us-west-2"
+  profile                  = "descomplicando_terraform"
+}
